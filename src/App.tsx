ccf44c04962f52ef3af750d6191bf036c48fe858
Clément Van Peuter <clement.vanpeuter@gmail.com>
2024-08-16 15:38:42 +0200

    add logo

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,25 @@ import { RouterProvider } from "react-router-dom";
 import router from "./router/router.tsx";
 
 const { Content, Footer, Sider } = Layout;
+import logo from "./assets/logo.png";
 
 const App: React.FC = () => {
   return (
     <Layout style={{ minHeight: "100vh" }}>
-      <Sider style={{ backgroundColor: "#5d576b" }}></Sider>
+      <Sider style={{ backgroundColor: "#d5cbbf" }}>
+        <div style={{ textAlign: "center" }}>
+          <img
+            src={logo}
+            style={{ height: "100px", width: "100px" }}
+            alt="logo"
+          />
+        </div>
+      </Sider>
       <Layout>
-        <Content style={{ backgroundColor: "#F1DEDE" }}>
+        <Content style={{ backgroundColor: "#c3fcf2" }}>
           <RouterProvider router={router} />
         </Content>
-        <Footer style={{ textAlign: "center", backgroundColor: "#6cd4ff" }}>
+        <Footer style={{ textAlign: "center", backgroundColor: "#649b92" }}>
           Pelycano Music
         </Footer>
       </Layout>
