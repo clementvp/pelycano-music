139c7ef4eafdbacf820dab6276a031f5cb9e8540
Clément Van Peuter <clement.vanpeuter@gmail.com>
2024-08-16 14:17:16 +0200

    add a home page

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home/Home.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Home />,
   },
 ]);
 
